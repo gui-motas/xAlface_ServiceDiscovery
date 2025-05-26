c811a765ed75f40b4db753919e06089cc49bae04
Silva, Guilherme (G.M.) <gsilv257+ford@ford.com>
2025-05-26 20:24:11 -0300

    Initial commit

---
+++ src/main/java/com/xalface/serviceDiscovery/eurekaServer/xAlface_ServiceDiscovery/XAlfaceServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.xalface.serviceDiscovery.eurekaServer.xAlface_ServiceDiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XAlfaceServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XAlfaceServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xalface/serviceDiscovery/eurekaServer/xAlface_ServiceDiscovery/XAlfaceServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xalface.serviceDiscovery.eurekaServer.xAlface_ServiceDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XAlfaceServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
