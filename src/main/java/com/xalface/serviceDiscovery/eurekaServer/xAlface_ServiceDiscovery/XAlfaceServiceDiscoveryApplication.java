c557a80775ea3662c8ef37ffd4b90d936603b498
monjogui <monjogui@gmail.com>
2025-05-28 01:29:11 +0000

    Eureka Server Set

--- src/main/java/com/xalface/serviceDiscovery/eurekaServer/xAlface_ServiceDiscovery/XAlfaceServiceDiscoveryApplication.java
+++ src/main/java/com/xalface/serviceDiscovery/eurekaServer/xAlface_ServiceDiscovery/XAlfaceServiceDiscoveryApplication.java
@@ -2,7 +2,10 @@ package com.xalface.serviceDiscovery.eurekaServer.xAlface_ServiceDiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class XAlfaceServiceDiscoveryApplication {
 
